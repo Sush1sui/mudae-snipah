61cd0fd17c94a9ff0f994b377be20f6c37161612
Sush1sui <jplovesraw@gmail.com>
2025-09-28 20:18:15 +0800

    refactor: read message history logger

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -42,6 +42,19 @@ func StartBot() {
 	}
 	defer sess.Close()
 
+	// Check read message history permission for MUDAE channel
+    mudaeChannel := os.Getenv("MUDAE_CHANNEL_ID")
+    if mudaeChannel == "" {
+        fmt.Println("MUDAE_CHANNEL_ID not set in environment")
+    } else {
+        // attempt to fetch a single message; lack of "Read Message History" or channel read will error
+        _, err := sess.ChannelMessages(mudaeChannel, 1, "", "", "")
+        if err != nil {
+            fmt.Printf("Warning: cannot read message history in channel %s: %v\n", mudaeChannel, err)
+        } else {
+            fmt.Printf("Read message history OK for channel %s\n", mudaeChannel)
+        }
+    }
 
 	// Deploy events
 	deploy.DeployEvents(sess)
