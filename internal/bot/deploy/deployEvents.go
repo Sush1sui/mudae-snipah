9ea82402b69c51ddc6e5e6f619a8d6f01a762667
Sush1sui <jplovesraw@gmail.com>
2025-09-28 20:44:07 +0800

    fix: forgot to add mudae event

--- internal/bot/deploy/deployEvents.go
+++ internal/bot/deploy/deployEvents.go
@@ -3,11 +3,12 @@ package deploy
 import (
 	"log"
 
+	"github.com/Sush1sui/sniper_bot/internal/bot/events"
 	"github.com/bwmarrin/discordgo"
 )
 
 var EventHandlers = []any{
-	
+	events.OnSnipeMudae,
 }
 
 func DeployEvents(sess *discordgo.Session) {
